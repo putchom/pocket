334fb06a39fb4997fe85f4542805c6abe1498b97
putchom <putchom8@gmail.com>
2021-10-23 13:11:30 +0900

    カウントした歩数をLCDに描画するところまで

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,52 @@ use wio_terminal as wio;
 
 use accelerometer::{vector::F32x3, Accelerometer};
 use core::fmt::Write;
-use eg::{fonts::*, pixelcolor::*, prelude::*, style::*};
+use eg::{
+    egrectangle, egtext, fonts::Font24x32, pixelcolor::*, prelude::*, primitive_style, text_style,
+};
+use heapless::consts::*;
+use heapless::String;
 use micromath::F32Ext;
 use wio::entry;
 use wio::hal::clock::GenericClockController;
 use wio::hal::delay::Delay;
 use wio::pac::{CorePeripherals, Peripherals};
 use wio::prelude::*;
+
+const SCREEN_WIDTH: i32 = 320;
+const SCREEN_HEIGHT: i32 = 240;
+
+fn draw_step_count<T>(display: &mut T, step_count: &mut i32) -> Result<(), T::Error>
+where
+    T: embedded_graphics::DrawTarget<Rgb565>,
+{
+    // カウント表示エリアをクリアする
+    const FONT_WIDTH: i32 = 24;
+    const FONT_HEIGHT: i32 = 32;
+    egrectangle!(
+        top_left = (0, 0),
+        bottom_right = (SCREEN_WIDTH - 1, FONT_HEIGHT),
+        style = primitive_style!(fill_color = Rgb565::WHITE)
+    )
+    .draw(display)?;
+
+    // 歩数を描画する
+    let mut textbuffer = String::<U256>::new();
+    write!(&mut textbuffer, "{:.2}", step_count).unwrap();
+
+    // 座標計算用に文字列の長さを取得
+    let length = textbuffer.len();
+    // 右詰描画用に左の座標計算
+    let left = SCREEN_WIDTH - (length as i32) * FONT_WIDTH;
+
+    egtext!(
+        text = textbuffer.as_str(),
+        top_left = (left, 0),
+        style = text_style!(font = Font24x32, text_color = Rgb565::BLACK)
+    )
+    .draw(display)?;
+    Ok(())
+}
 
 #[entry]
 fn main() -> ! {
@@ -45,6 +84,17 @@ fn main() -> ! {
         )
         .unwrap();
 
+    // ディスプレイの初期化
+    egrectangle!(
+        top_left = (0, 0),
+        bottom_right = (SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1),
+        style = primitive_style!(fill_color = Rgb565::WHITE)
+    )
+    .draw(&mut display)
+    .unwrap();
+    // 歩数カウントも初期化
+    draw_step_count(&mut display, &mut 0).unwrap();
+
     // UARTドライバオブジェクトを初期化する
     let mut serial = sets.uart.init(
         &mut clocks,
@@ -61,12 +111,6 @@ fn main() -> ! {
         &mut peripherals.MCLK,
         &mut sets.port,
     );
-
-    // 画面に「Hello world!」と表示する
-    Text::new("Hello world!", Point::new(30, 30))
-        .into_styled(TextStyle::new(Font12x16, Rgb565::BLACK))
-        .draw(&mut display)
-        .unwrap();
 
     let mut count = 0;
     let mut total = 0.0;
@@ -104,11 +148,11 @@ fn main() -> ! {
         if !last_state && state {
             step_count += 1;
             last_state = state;
+            draw_step_count(&mut display, &mut step_count).unwrap();
         } else if last_state && !state {
             last_state = state;
         }
 
-        writeln!(&mut serial, "Step count: {:.2}", step_count).unwrap();
         delay.delay_ms(100u16);
     }
 }

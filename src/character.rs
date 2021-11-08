805a85d3515674ebbc0e717efbdb5a56311f3bb7
putchom <putchom8@gmail.com>
2021-11-08 14:22:33 +0900

    直接受けるほうがRustっぽさがある

--- src/character.rs
+++ src/character.rs
@@ -22,17 +22,15 @@ impl Character {
         const WIDTH: u32 = 180;
         const HEIGHT: u32 = 117;
 
-        let data;
-
-        match self.state {
-            CharacterState::Angry => data = include_bytes!("./assets/character/angry/default.raw"),
-            CharacterState::Away => data = include_bytes!("./assets/character/away/default.raw"),
-            CharacterState::Eat => data = include_bytes!("./assets/character/eat/default.raw"),
-            CharacterState::Happy => data = include_bytes!("./assets/character/happy/default.raw"),
-            CharacterState::Play => data = include_bytes!("./assets/character/play/default.raw"),
-            CharacterState::Shy => data = include_bytes!("./assets/character/shy/default.raw"),
-            CharacterState::Sleep => data = include_bytes!("./assets/character/sleep/default.raw"),
-        }
+        let data = match self.state {
+            CharacterState::Angry => include_bytes!("./assets/character/angry/default.raw"),
+            CharacterState::Away => include_bytes!("./assets/character/away/default.raw"),
+            CharacterState::Eat => include_bytes!("./assets/character/eat/default.raw"),
+            CharacterState::Happy => include_bytes!("./assets/character/happy/default.raw"),
+            CharacterState::Play => include_bytes!("./assets/character/play/default.raw"),
+            CharacterState::Shy => include_bytes!("./assets/character/shy/default.raw"),
+            CharacterState::Sleep => include_bytes!("./assets/character/sleep/default.raw"),
+        };
 
         ImageRawLE::new(data, WIDTH, HEIGHT)
     }

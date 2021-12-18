15b09c9474e4bd2f4112b2a545366bf8a7527d61
putchom <putchom8@gmail.com>
2021-12-18 16:46:27 +0900

    的の画像を置き換え

--- pocket/src/views/pages/game_page.rs
+++ pocket/src/views/pages/game_page.rs
@@ -7,7 +7,7 @@ use embedded_graphics::{
     prelude::*,
 };
 
-const ICON_SIZE: i32 = 32;
+const TARGET_SIZE: i32 = 32;
 
 pub struct GamePage;
 
@@ -23,44 +23,44 @@ impl GamePage {
 
         // 左の的を描画
         let left_data = match target_position {
-            TargetPosition::Left => include_bytes!("../../assets/navigation/play.raw"),
-            TargetPosition::Center => include_bytes!("../../assets/navigation/home.raw"),
-            TargetPosition::Right => include_bytes!("../../assets/navigation/home.raw")
+            TargetPosition::Left => include_bytes!("../../assets/game/shuriken.raw"),
+            TargetPosition::Center => include_bytes!("../../assets/game/empty_target.raw"),
+            TargetPosition::Right => include_bytes!("../../assets/game/empty_target.raw")
         };
         let left_image_data = ImageRawLE::new(
             left_data,
-            ICON_SIZE.try_into().unwrap(),
-            ICON_SIZE.try_into().unwrap(),
+            TARGET_SIZE.try_into().unwrap(),
+            TARGET_SIZE.try_into().unwrap(),
         );
-        let left_point = Point::new(0, ICON_SIZE);
+        let left_point = Point::new(0, TARGET_SIZE);
         Image::new(&left_image_data, left_point).draw(display)?;
 
         // 中央の的を描画
         let center_data = match target_position {
-            TargetPosition::Left => include_bytes!("../../assets/navigation/home.raw"),
-            TargetPosition::Center => include_bytes!("../../assets/navigation/play.raw"),
-            TargetPosition::Right => include_bytes!("../../assets/navigation/home.raw")
+            TargetPosition::Left => include_bytes!("../../assets/game/target.raw"),
+            TargetPosition::Center => include_bytes!("../../assets/game/shuriken.raw"),
+            TargetPosition::Right => include_bytes!("../../assets/game/target.raw")
         };
         let center_image_data = ImageRawLE::new(
             center_data,
-            ICON_SIZE.try_into().unwrap(),
-            ICON_SIZE.try_into().unwrap(),
+            TARGET_SIZE.try_into().unwrap(),
+            TARGET_SIZE.try_into().unwrap(),
         );
-        let center_point = Point::new(ICON_SIZE, ICON_SIZE);
+        let center_point = Point::new(TARGET_SIZE, TARGET_SIZE);
         Image::new(&center_image_data, center_point).draw(display)?;
 
         // 右の的を描画
         let right_data = match target_position {
-            TargetPosition::Left => include_bytes!("../../assets/navigation/home.raw"),
-            TargetPosition::Center => include_bytes!("../../assets/navigation/home.raw"),
-            TargetPosition::Right => include_bytes!("../../assets/navigation/play.raw")
+            TargetPosition::Left => include_bytes!("../../assets/game/empty_target.raw"),
+            TargetPosition::Center => include_bytes!("../../assets/game/empty_target.raw"),
+            TargetPosition::Right => include_bytes!("../../assets/game/shuriken.raw")
         };
         let right_image_data = ImageRawLE::new(
             right_data,
-            ICON_SIZE.try_into().unwrap(),
-            ICON_SIZE.try_into().unwrap(),
+            TARGET_SIZE.try_into().unwrap(),
+            TARGET_SIZE.try_into().unwrap(),
         );
-        let right_point = Point::new(ICON_SIZE * 2, ICON_SIZE);
+        let right_point = Point::new(TARGET_SIZE * 2, TARGET_SIZE);
         Image::new(&right_image_data, right_point).draw(display)?;
 
         Ok(())
